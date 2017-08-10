35f952d317df44bad0d0c18a06149d2fb8e8f995
MIYAGI Hikaru <hikaru.devel@js4.in>
2017-08-11 00:39:09 +0900

    エンディアンネス検出のバグ修正

--- endianness-check.sh
+++ endianness-check.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-val=$(printf '\1\0\0\0' |od -v -An -txI)
-if [ $val -eq 1 ]
+val=$(printf '\1\2\3\4' |od -v -An -tuI)
+if [ $val -eq $((0x04030201)) ]
 then
 	echo '#define LITTLE_ENDIAN'
-elif [ $val -eq 16777216 ]
+elif [ $val -eq $((0x01020304)) ]
 then
 	:
 else
