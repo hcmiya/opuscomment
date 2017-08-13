191df59544c8049c9493b3f79620fb391c35af30
MIYAGI Hikaru <hikaru.devel@js4.in>
2017-08-13 15:00:40 +0900

    変なエンディアンネスのマシンでOpusの整数をパースできるようにする

--- endianness-check.sh
+++ endianness-check.sh
@@ -2,6 +2,7 @@
 val=$(printf '\20\21\22\23\24\25\26\27' |od -v -An -tx8)
 if [ $val = 1716151413121110 ]
 then
+	#little endian
 	cat <<heredoc
 static inline uint16_t oi16(uint16_t i) {
 	return i;
@@ -15,6 +16,7 @@ static inline uint64_t oi64(uint64_t i) {
 heredoc
 elif [ $val = 1011121314151617 ]
 then
+	#big engian
 	cat <<heredoc
 #define M 255ULL
 
@@ -31,5 +33,19 @@ static inline uint64_t oi64(uint64_t i) {
 #undef M
 heredoc
 else
-	echo '#error 謎のエンディアンネス'
+	terrible_endianness() {
+		cat <<heredoc
+static inline uint${1}_t oi${1}(uint${1}_t i) {
+	uint8_t *val = &i;
+	uint8_t out[$2];
+heredoc
+		printf "$3" |od -v -An -tx$2 |grep -o '1[0-7]' |awk 'BEGIN{NR--}{printf("\tout[%d] = val[%d];\n", $1 - 10, NR)}'
+		cat <<heredoc
+	return *(uint${1}_t*)out;
+}
+heredoc
+	}
+	terrible_endianness 16 2 '\21\20'
+	terrible_endianness 32 4 '\23\22\21\20'
+	terrible_endianness 64 8 '\27\26\25\24\23\22\21\20'
 fi
