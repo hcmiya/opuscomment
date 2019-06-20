922162a4c7d411266f3b0ca5041fa7dbffcded10
zvezdochiot <mykaralw@yandex.ru>
2019-06-20 13:19:26 +0300

    1.5.10: join opuscomment.h and ocutil.h

--- src/opuscomment.h
+++ src/opuscomment.h
@@ -39,7 +39,6 @@
 #endif
 
 #include "global.h"
-#include "ocutil.h"
 #include "limit.h"
 #include "error.h"
 
@@ -77,4 +76,19 @@ void store_tags(ogg_page *np, struct rettag_st *rst, struct edit_st *est, bool p
 void *retrieve_tags(void*);
 void rt_del_args(uint8_t *buf, size_t len, bool term);
 
+uint16_t oi16(uint16_t i);
+uint32_t oi32(uint32_t i);
+uint64_t oi64(uint64_t i);
+bool test_tag_field(uint8_t *line, size_t n, bool upcase, bool *on_field, bool *upcase_applied);
+size_t fill_buffer(void *buf, size_t left, size_t buflen, FILE *fp);
+bool test_non_opus(ogg_page *og);
+void write_page(ogg_page *og, FILE *fp);
+void set_pageno(ogg_page *og, int no);
+void set_granulepos(ogg_page *og, int64_t pos);
+
+#if _POSIX_C_SOURCE < 200809L
+size_t strnlen(char const *src, size_t n);
 #endif
+
+#endif //end OPUSCOMMENT_H
+
